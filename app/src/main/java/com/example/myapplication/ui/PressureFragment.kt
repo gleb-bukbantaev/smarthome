27bdf4637efa2565dcda8a3c5b8dd7e3dceb3cb6
Memento <nikole.gurtovenko@mail.ru>
2020-08-05 17:30:48 +0700

    PressureFragment.kt

--- app/src/main/java/com/example/myapplication/ui/PressureFragment.kt
+++ app/src/main/java/com/example/myapplication/ui/PressureFragment.kt
@@ -7,12 +7,14 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.myapplication.R
 
-class LightFragment: Fragment() {
+class PressureFragment :Fragment()
+{
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_light, container, false)
+        return inflater.inflate(R.layout.fragment_pressure, container, false)
+
     }
 }
--- app/src/main/java/com/example/myapplication/ui/TemperatureInsideFragment.kt
+++ app/src/main/java/com/example/myapplication/ui/TemperatureInsideFragment.kt
@@ -7,12 +7,14 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.myapplication.R
 
-class LightFragment: Fragment() {
+class TemperatureInsideFragment:Fragment()
+{
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_light, container, false)
+        return inflater.inflate(R.layout.fragment_temperature_inside, container, false)
+
     }
 }
