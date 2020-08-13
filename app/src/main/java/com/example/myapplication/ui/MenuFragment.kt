83d1fa9cde0a1549928a771d65876474973322a5
Memento <nikole.gurtovenko@mail.ru>
2020-08-13 11:07:50 +0700

    HumidityState.kt LightState.kt

--- app/src/main/java/com/example/myapplication/ui/MenuFragment.kt
+++ app/src/main/java/com/example/myapplication/ui/MenuFragment.kt
@@ -56,8 +56,8 @@ class MenuFragment: Fragment() {
             pressureValue = state.pressureValue
             windowState = state.windowState
             lockState = state.lockState
-            come_light.imageTintList=if (lightState==true){
-                ColorStateList.valueOf(Color.parseColor("#ffff00"))}
+            come_light.imageTintList
+
 
                 come_humidity.text = "$humidityState | $humidityValue"
             come_lock.text = "$lockState"
